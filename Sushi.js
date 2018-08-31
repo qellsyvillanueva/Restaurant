7eaaece9afbea988b29f0c96a56fd3ba2833a090
qellsyvillanueva <42422251+qellsyvillanueva@users.noreply.github.com>
2018-08-31 12:19:46 -1000

    Add files via upload

---
+++ Sushi.js
@@ -0,0 +1,16 @@
+var spicyAhi = {
+fishType: "Ahi",
+sauce: "Sriracha & Mayo",
+price: 100.00,
+rice: "White",
+ounces: 8
+};
+
+//Ask user for amount of spicy ahi to buy
+var amount = prompt("How much are you ordering?");
+//Display the total amount of ounces the user will buy
+var totalOunces = amount * spicyAhi.ounces;
+window.alert("Purchasing: " + totalOunces);
+//then display the total dollar amount
+var totalDollar = amount * spicyAhi.price;
+window.alert("Your total price: " + totalDollar);
